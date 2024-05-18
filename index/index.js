2207cca9d6dc7b2ab4dd6b6731473c4c37c69334
Daniel <daniel.junior137@hotmail.com>
2024-05-18 15:54:39 -0300

    tirando o efeito de rolagem da página

--- index/index.js
+++ index/index.js
@@ -1,4 +1,4 @@
-window.addEventListener("scroll", function(){
-    let header = document.querySelector('.cabecalho')
-    header.classList.toggle('menu-rolagem',window.scrollY > 0)
-})
+// window.addEventListener("scroll", function(){
+//     let header = document.querySelector('.cabecalho')
+//     header.classList.toggle('menu-rolagem',window.scrollY > 0)
+// })
