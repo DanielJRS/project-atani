9ded0e9deeb777a979489fc39f93c0148644970e
Daniel <daniel.junior137@hotmail.com>
2024-06-18 20:45:11 -0300

    adicionando parcialmente a responsividade da index e implementando o html e css da página sobre nós.

--- index/index.js
+++ index/index.js
@@ -2,3 +2,12 @@
 //     let header = document.querySelector('.cabecalho')
 //     header.classList.toggle('menu-rolagem',window.scrollY > 0)
 // })
+
+//Menu responsivo
+// function clickMenu() {
+//     if (menu.style.display == 'flex') {
+//         menu.style.display = 'none'
+//     } else {
+//         menu.style.display = 'flex'
+//     }
+// }
